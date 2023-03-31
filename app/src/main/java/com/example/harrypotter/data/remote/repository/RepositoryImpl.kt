96b257df8e34278b784212d87c08a1c617f806cd
BENJAH <benjahjp@gmail.com>
2023-03-31 11:49:05 +0300

    feat: repository implementation

--- app/src/main/java/com/example/harrypotter/data/remote/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/harrypotter/data/remote/repository/RepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.example.harrypotter.data.remote.repository
 
-class RepositoryImpl {
+import com.example.harrypotter.data.remote.api.Api
+import com.example.harrypotter.data.remote.dto.SingleCharacter
+import com.example.harrypotter.domain.repository.Repository
+
+class RepositoryImpl(
+    val api: Api
+) : Repository {
+    override suspend fun getAllCharacters(): List<SingleCharacter> {
+        return api.getAllCharacters()
+    }
+
+    override suspend fun getCharacterByHouse(houseName: String): List<SingleCharacter> {
+        return api.getCharacterByHouseName(houseName)
+    }
 }
