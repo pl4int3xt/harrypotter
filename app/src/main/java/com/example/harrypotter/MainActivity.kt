e187cd79272c1e469cf3f1eeceae0fda4a0780aa
BENJAH <benjahjp@gmail.com>
2023-04-01 11:00:23 +0300

    feat: splash and decor

--- app/src/main/java/com/example/harrypotter/MainActivity.kt
+++ app/src/main/java/com/example/harrypotter/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.example.harrypotter.presentation.home.components.HomeScreen
 import com.example.harrypotter.presentation.navigation.MainNavGraph
 import com.example.harrypotter.presentation.theme.HarrypotterTheme
@@ -15,6 +17,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             HarrypotterTheme {
                 MainNavGraph(navHostController = rememberAnimatedNavController())
