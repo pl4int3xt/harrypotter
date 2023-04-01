b107ac5c5600cf65d138ef2e854f5ce3e0d05593
BENJAH <benjahjp@gmail.com>
2023-04-01 11:31:04 +0300

    feat: animated navigation

--- app/src/main/java/com/example/harrypotter/presentation/navigation/MainNavGraph.kt
+++ app/src/main/java/com/example/harrypotter/presentation/navigation/MainNavGraph.kt
@@ -1,6 +1,8 @@
 package com.example.harrypotter.presentation.navigation
 
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.example.harrypotter.domain.model.SingleCharacterModel
@@ -19,8 +21,40 @@ fun MainNavGraph(
         navController = navHostController,
         startDestination = Screens.HomeScreen.route
     ){
-        composable(route = Screens.HomeScreen.route){ HomeScreen(navHostController = navHostController)}
-        composable(route = Screens.DetailsScreen.route){
+        composable(
+            route = Screens.HomeScreen.route,
+            enterTransition = {
+                when (targetState.destination.route) {
+                    navHostController.currentDestination?.route ->
+                        slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(300))
+                    else -> null
+                }
+            },
+            popExitTransition = {
+                when (targetState.destination.route) {
+                    navHostController.currentDestination?.route ->
+                        slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(1000))
+                    else -> null
+                }
+            }
+        ){ HomeScreen(navHostController = navHostController)}
+        composable(
+            route = Screens.DetailsScreen.route,
+            enterTransition = {
+                when (targetState.destination.route) {
+                    navHostController.currentDestination?.route ->
+                        slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(300))
+                    else -> null
+                }
+            },
+            popExitTransition = {
+                when (targetState.destination.route) {
+                    navHostController.currentDestination?.route ->
+                        slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(1000))
+                    else -> null
+                }
+            }
+        ){
             val characterDetails = navHostController.previousBackStackEntry?.savedStateHandle?.get<SingleCharacterModel>("details")
             if (characterDetails != null) {
                 DetailsScreen(characterDetails, navHostController)
