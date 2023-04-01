63e075b759636e00d5abcd99620df7fda15a904d
BENJAH <benjahjp@gmail.com>
2023-04-01 10:55:50 +0300

    feat: navigation and detail screen

--- app/src/main/java/com/example/harrypotter/MainActivity.kt
+++ app/src/main/java/com/example/harrypotter/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.harrypotter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import com.example.harrypotter.presentation.home.components.HomeScreen
+import com.example.harrypotter.presentation.navigation.MainNavGraph
 import com.example.harrypotter.presentation.theme.HarrypotterTheme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HarrypotterTheme {
-                HomeScreen()
+                MainNavGraph(navHostController = rememberAnimatedNavController())
             }
         }
     }
