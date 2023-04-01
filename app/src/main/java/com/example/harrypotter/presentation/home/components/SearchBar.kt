5c810b87ae7675404465a1693f2173e4fbba32b5
BENJAH <benjahjp@gmail.com>
2023-04-01 11:42:22 +0300

    feat: search bar

--- app/src/main/java/com/example/harrypotter/presentation/home/components/SearchBar.kt
+++ app/src/main/java/com/example/harrypotter/presentation/home/components/SearchBar.kt
@@ -1,16 +1,54 @@
 package com.example.harrypotter.presentation.home.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
-fun SearchBar() {
+fun SearchBar(
+    onSearch: () -> Unit,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     TextField(
-        value = "",
-        onValueChange = {}
+        modifier = Modifier.clip(RoundedCornerShape(15.dp)),
+        value = value,
+        placeholder = { Text(text = "search here ...") },
+        onValueChange = { onValueChange(it) },
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = MaterialTheme.colorScheme.tertiary,
+            cursorColor = MaterialTheme.colorScheme.primary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        ),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearch()
+                keyboardController?.hide()
+            }
+        )
     )
 }
 
