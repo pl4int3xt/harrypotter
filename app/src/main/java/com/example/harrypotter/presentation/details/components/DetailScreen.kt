0ca81944d229f43e64bebf14661d07f7cf89c8b7
BENJAH <benjahjp@gmail.com>
2023-04-01 11:25:38 +0300

    fix: Detail Screen

--- app/src/main/java/com/example/harrypotter/presentation/details/components/DetailScreen.kt
+++ app/src/main/java/com/example/harrypotter/presentation/details/components/DetailScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,7 +57,7 @@ fun DetailsScreen(
                     )
                 }
                 Column(
-                    modifier = Modifier
+                    modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
                 ) {
                     Column(
                         modifier = Modifier
--- app/src/main/java/com/example/harrypotter/presentation/details/components/DetailsRow.kt
+++ app/src/main/java/com/example/harrypotter/presentation/details/components/DetailsRow.kt
@@ -2,10 +2,12 @@ package com.example.harrypotter.presentation.details.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -17,13 +19,14 @@ fun DetailsRow(
         Text(
             text = title,
             fontWeight = FontWeight.Bold,
-            fontSize = 25.sp
+            fontSize = 20.sp
         )
         Spacer(modifier = Modifier.weight(1f))
         Text(
             text = value.toString(),
             fontWeight = FontWeight.Bold,
-            fontSize = 25.sp
+            fontSize = 20.sp
         )
     }
+    Spacer(modifier = Modifier.height(5.dp))
 }
