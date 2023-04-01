377fa4e93ed21ec6697cbb2489bb8b8394c68330
BENJAH <benjahjp@gmail.com>
2023-04-01 11:52:54 +0300

    feat: search bar

--- app/src/main/java/com/example/harrypotter/presentation/home/components/SearchBar.kt
+++ app/src/main/java/com/example/harrypotter/presentation/home/components/SearchBar.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
