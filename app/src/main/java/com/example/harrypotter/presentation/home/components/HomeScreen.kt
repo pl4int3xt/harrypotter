0d3252b304165ea5a36c39e8964d9cca3bf2c05a
BENJAH <benjahjp@gmail.com>
2023-04-02 18:40:39 +0300

    feat: search

--- app/src/main/java/com/example/harrypotter/presentation/home/components/HomeScreen.kt
+++ app/src/main/java/com/example/harrypotter/presentation/home/components/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
@@ -20,7 +21,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.harrypotter.presentation.details.components.NavigationButton
@@ -50,7 +53,13 @@ fun HomeScreen(
                         onClick = { navHostController.navigate(Screens.SearchScreen.route) },
                         icon = Icons.Rounded.Search)
                 },
-                title = {}
+                title = {
+                    Text(
+                        text = "Harry Potter",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp
+                    )
+                }
             )
         }
     ) {
