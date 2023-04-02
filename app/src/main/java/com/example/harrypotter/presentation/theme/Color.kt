20edc1a107cd93a1334627b6e39ef2cfc16c167a
BENJAH <benjahjp@gmail.com>
2023-04-02 19:38:14 +0300

    fix

--- app/src/main/java/com/example/harrypotter/presentation/theme/Color.kt
+++ app/src/main/java/com/example/harrypotter/presentation/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.harrypotter.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFF00C853)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF64DD17)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
